7d0214c7efcefb79dc1420e19354b9bc8673658f
Saeful Rohman <Saefulgaming72@gmail.com>
2025-01-15 15:06:12 +0700

    update production

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://alope.id/skillbridge.php/api/service",
+  baseURL: "https://alope.id/skillbridge.php/api/",
   headers: {
     "Content-Type": "application/json",
   },
